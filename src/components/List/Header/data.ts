601f51a01d16e134cd3683450884e670e96ad4e1
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2024-07-08 17:15:18 -0300

    deleted

--- src/components/List/Header/data.ts
+++
@@ -1,4 +0,0 @@
-export interface IHeaderProps {
-  tasksCounter: number
-  checkedTasksCounter: number
-}
--- src/components/List/Item/data.ts
+++
@@ -1,7 +0,0 @@
-import {ITask} from '@/data'
-
-export interface IItemProps {
-  data: ITask
-  removeTask: (id: number) => void
-  toggleTaskStatus: ({id, value}: {id: number; value: boolean}) => void
-}
